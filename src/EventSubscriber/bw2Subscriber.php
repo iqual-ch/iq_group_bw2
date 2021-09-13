d47d0d7afce6f803b5a6b4c804e36e5503516a46
Guillaume Pacilly <guillaume.pacilly@gmail.com>
2021-09-13 12:06:01 +0200

    passing array instead of user object

--- src/EventSubscriber/bw2Subscriber.php
+++ src/EventSubscriber/bw2Subscriber.php
@@ -51,11 +51,47 @@ class bw2Subscriber implements EventSubscriberInterface {
       /** @var \Drupal\user\UserInterface $user */
       $user = $event->getUser();
       if ($user->hasField('field_iq_group_bw2_id') && !empty($user->get('field_iq_group_bw2_id')->getValue())) {
-        $this->bw2ApiService->editContact($user->get('field_iq_group_bw2_id')->getValue(), $user);
+        $profile_data = $this->convertDataForBw2($user);
+        $this->bw2ApiService->editContact($user->get('field_iq_group_bw2_id')->getValue(), $profile_data);
       } else {
-        $bw2_id = $this->bw2ApiService->createContact($user);
+        $bw2_id = $this->bw2ApiService->createContact($profile_data);
         $user->set('field_iq_group_bw2_id', $bw2_id);
       }
     }
   }
+
+  /**
+   * Helper function to convert the user data to the bw2 format.
+   */
+  public function convertDataForBw2($user){
+    $langCode = $this->bw2ApiService->getLanguageCode($user->getPreferredLangcode());
+    $countryCode = $this->bw2ApiService->getCountryCode(reset($user->get('field_iq_user_base_address')->getValue())['country_code']);
+    $profile_data = [
+      'Account_Active' => $user->status->value,
+      // 'Account_Salutation' => reset($user->get('field_iq_user_base_address')->getValue())['given_name'],
+      // 'Account_Drupal_ID' => $user->id(),
+      'Account_FirstName' => reset($user->get('field_iq_user_base_address')->getValue())['given_name'],
+      'Account_LastName' => reset($user->get('field_iq_user_base_address')->getValue())['family_name'],
+      // 'Account_AddressLine1' => reset($user->get('field_iq_user_base_address')->getValue())['address_line1'],
+      'Account_Street' => reset($user->get('field_iq_user_base_address')->getValue())['address_line1'],
+      // 'Account_POBox' => reset($user->get('field_iq_user_base_address')->getValue())['street'],
+      'Account_PostalCode' => reset($user->get('field_iq_user_base_address')->getValue())['postal_code'],
+      'Account_City' => reset($user->get('field_iq_user_base_address')->getValue())['locality'],
+      'Account_Country_Dimension_ID' => $countryCode,
+      'Account_Email1' => $user->getEmail(),
+      'Account_Language_Dimension_ID' => $langCode
+    ];
+
+    if ($user->hasField('field_iq_group_preferences') && !$user->get('field_iq_group_preferences')->isEmpty()) {
+      $profile_data['Visitor_AllowEmail'] = array_filter(array_column($user->field_iq_group_preferences->getValue(), 'target_id'));
+    }
+    // if ($user->hasField('field_iq_group_bw2_id') && !empty($user->get('field_iq_group_bw2_id')->getValue())) {
+    //   $this->bw2ApiService->editContact($user->field_iq_group_bw2_id->value, $profile_data);
+    // } else {
+    //   $bw2_id = $this->bw2ApiService->createContact($email, $profile_data);
+    //   $user->set('field_iq_group_bw2_id', $bw2_id);
+    // }
+    return $profile_data;
+  }
+
 }
