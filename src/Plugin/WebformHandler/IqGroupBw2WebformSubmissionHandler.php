b12b31fc0f13443cae5c9832a8cd1b4d1950f938
Guillaume Pacilly <guillaume.pacilly@gmail.com>
2021-09-23 15:08:46 +0200

    Connection with Webform

--- src/Plugin/WebformHandler/IqGroupBw2WebformSubmissionHandler.php
+++ src/Plugin/WebformHandler/IqGroupBw2WebformSubmissionHandler.php
@@ -29,53 +29,69 @@ class IqGroupBw2WebformSubmissionHandler extends \Drupal\webform\Plugin\WebformH
 
     $user_data = [];
     $userExists = TRUE;
-    $values = $webform_submission->getData();
 
-    $email = '';
     $user = NULL;
-    foreach ($form['elements'] as $key => $element) {
-      if ($key == 'customer_email') {
-        $user = \Drupal::entityTypeManager()->getStorage('user')->loadByProperties(
-          [
-            'mail' => $form_state->getValue($key)
-          ]
-        );
-        if (count($user) == 0){
-          $userExists = FALSE;
 
-          $user_data['name'] = $form_state->getValue($key);
-          $user_data['mail'] = $user_data['name'];
-          $currentLanguage = $language = \Drupal::languageManager()->getCurrentLanguage()->getId();;
-          $user_data['preferred_langcode'] = $currentLanguage;
-          $user_data['langcode'] = $currentLanguage;
-        }
-        else {
-          $user = reset($user);
-          $email = $user->getEmail();
-        }
+    if ($form_state->getValue('customer_email')) {
+      $user = \Drupal::entityTypeManager()->getStorage('user')->loadByProperties(
+        [
+          'mail' => $form_state->getValue('customer_email')
+        ]
+      );
+      if (count($user) == 0){
+        $userExists = FALSE;
+        $user_data['name'] = $form_state->getValue('customer_email');
+        $user_data['mail'] = $user_data['name'];
+        $currentLanguage = \Drupal::languageManager()->getCurrentLanguage()->getId();;
+        $user_data['preferred_langcode'] = $currentLanguage;
+        $user_data['langcode'] = $currentLanguage;
       }
-      else if ($form_state->getValue($key) && $key == 'newsletter_subscription') {
-        $user_data['field_iq_group_preferences'] = $form_state->getValue($key);
-      }
-      elseif ($form_state->getValue($key) && $key == 'customer_salutation') {
-        $user_data['field_iq_user_base_salutation'] = $form_state->getValue($key);
+      else {
+        $user = reset($user);
       }
     }
-    // Set the country code to Switzerland as it is required.
-    $user_data['field_iq_user_base_address']['country_code'] = 'CH';
+    if ($form_state->getValue('customer_salutation')) {
+      $user_data['field_iq_user_base_salutation'] = $form_state->getValue('customer_salutation');
+    }
+    if ($form_state->getValue('customer_first_name')) {
+      $user_data['field_iq_user_base_address']['given_name'] = $form_state->getValue('customer_first_name');
+    }
+    if ($form_state->getValue('customer_last_name')) {
+      $user_data['field_iq_user_base_address']['family_name'] = $form_state->getValue('customer_last_name');
+    }
+    if ($form_state->getValue('customer_address')) {
+      $user_data['field_iq_user_base_address']['address_line1'] =  $form_state->getValue('customer_address')['address'];
+      $user_data['field_iq_user_base_address']['address_line2'] =  $form_state->getValue('customer_address')['address_2'];
+      $user_data['field_iq_user_base_address']['locality'] =  $form_state->getValue('customer_address')['city'];
+      $user_data['field_iq_user_base_address']['postal_code'] =  $form_state->getValue('customer_address')['postal_code'];
+      $user_data['field_iq_user_base_address']['country_code'] =  $form_state->getValue('customer_address')['country'];
+    }
+    if ($form_state->getValue('customer_birth_date')) {
+      $user_data['field_gcb_custom_birth_date'] = $form_state->getValue('customer_birth_date');
+    }
 
-    
+    $user_data['field_iq_group_preferences'] = ($form_state->getValue('customer_newsletter')) ? [1,2] : [1];
+
+    // Set the country code to Switzerland as it is required.
+    if (!empty($user_data['field_iq_user_base_address']['country_code'])){
+       $user_data['field_iq_user_base_address']['country_code'] = 'CH';
+    }
+
     // If user exists, attribute the submission to the user.
     if (!empty($user) && $userExists) {
         $webform_submission->setOwnerId($user->id())->save();
     }
-    // If the user does not exists and the user checked the newsletter,
-    // Create the user and attribute the submission to the user.
-    else if (!empty($user_data['field_iq_group_preferences'])) {
+    // If the user does not exists,
+    // Create the user, register him to the iq group and attribute the submission to the user.
+    else  {
       $user = UserController::createMember($user_data);
       $store = \Drupal::service('user.shared_tempstore')->get('iq_group.user_status');
       $store->set($user->id().'_pending_activation', true);
       $webform_submission->setOwnerId($user->id())->save();
+      $group_general = \Drupal\group\Entity\Group::load(1);
+      \Drupal\iq_group\Controller\UserController::addGroupRoleToUser($group_general, $user, 'subscription-subscriber');
+      \Drupal::logger('iq_group_bw2')->notice('user added to the general group');
     }
+
   }
 }
